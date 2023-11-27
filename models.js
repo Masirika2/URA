4869ebde5973609e32af70c0c37e0761dbaaa716
Masirika2 <91177805+Masirika2@users.noreply.github.com>
2023-11-27 18:45:37 +0300

    Update models.js

--- models.js
+++ models.js
@@ -1,1 +1,30 @@
 //For models
+const mongoose = require('mongoose');
+
+// TaxPayer schema
+const taxPayerSchema = new mongoose.Schema({
+  fullName: { type: String, required: true },
+  dateOfBirth: { type: Date, required: true },
+  phoneNumber: { type: String, required: true },
+  address: { type: String, required: true },
+  tin: { type: String, required: true, unique: true },
+});
+
+// Asset schema
+const assetSchema = new mongoose.Schema({
+  assetName: { type: String, required: true },
+  estimatedCost: { type: Number, required: true },
+  ownersTIN: { type: String, required: true },
+  assetType: { type: String, enum: ['Fixed', 'Movable'], required: true },
+  assetCode: { type: String, required: true, unique: true },
+});
+
+// Create models
+const TaxPayer = mongoose.model('TaxPayer', taxPayerSchema);
+const Asset = mongoose.model('Asset', assetSchema);
+
+// Export models
+module.exports = {
+  TaxPayer,
+  Asset,
+};
